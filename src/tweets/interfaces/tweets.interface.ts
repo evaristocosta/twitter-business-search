984ca5d48829c58b9ba98e22df45e56418253e87
Lucas Costa <lucascosta74@gmail.com>
2022-01-03 19:54:46 -0300

    resposta formatada

--- src/tweets/interfaces/tweets.interface.ts
+++ src/tweets/interfaces/tweets.interface.ts
@@ -1,9 +1,18 @@
-interface TweetData {
+export interface TweetData {
   author_id: string;
   id: string;
   text: string;
 }
 
+export interface AuthorData {
+  id: string;
+  name: string;
+  username: string;
+}
+
 export interface Tweets {
   data: Array<TweetData>;
+  includes: {
+    users: Array<AuthorData>;
+  };
 }
--- src/tweets/tweets.service.ts
+++ src/tweets/tweets.service.ts
@@ -1,4 +1,4 @@
-import { Tweets } from './interfaces/tweets.interface';
+import { Tweets, TweetData, AuthorData } from './interfaces/tweets.interface';
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
@@ -9,14 +9,37 @@ export class TweetsService {
   constructor(private httpService: HttpService) {}
 
   findUser(business: string): Observable<AxiosResponse<Tweets[]>> {
+    const urlMentionsQuery =
+      'expansions=author_id&user.fields=name&max_results=20';
+
     return this.httpService.get('users/by/username/' + business).pipe(
       map((response) => response.data),
       switchMap((userData) =>
         this.httpService.get(
-          `users/${userData.data.id}/mentions?expansions=author_id&user.fields=name&max_results=20`,
+          `users/${userData.data.id}/mentions?${urlMentionsQuery}`,
         ),
       ),
-      map((response) => response.data),
+      map((response) => {
+        const resp = response.data;
+        const tweets = resp.data;
+        const authors = resp.includes.users;
+
+        // merge author data with tweet data matching author_id
+        const mergedTweets = tweets.map((tweet: TweetData) => {
+          const author = authors.find(
+            (author: AuthorData) => author.id === tweet.author_id,
+          );
+          return {
+            tweet_id: tweet.id,
+            tweet_text: tweet.text,
+            author_name: author.name,
+            author_id: author.id,
+            author_username: author.username,
+          };
+        });
+
+        return mergedTweets;
+      }),
     );
   }
 }
